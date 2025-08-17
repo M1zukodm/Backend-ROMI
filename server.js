2c5f01a3699771b25f90fd0b8ca11e5d7c9fc2a6
Miguel Cruz <18276841@uagro.mx>
2025-08-17 16:21:54 -0600

    Cambios

--- server.js
+++ server.js
@@ -157,7 +157,7 @@ const swaggerOptions = {
       description: "API para gestionar síntomas y registrar pacientes con recomendaciones médicas.",
     },
     servers: [
-      { url: "https://backend-romi.vercel.app", description: "Producción (Vercel)" }, // URL de producción
+      { url: "https://backend-romi.vercel.app/api-docs/", description: "Producción (Vercel)" }, // URL de producción
       { url: "http://localhost:3000", description: "Entorno local" }                   //  URL de desarrollo
     ],
   },
